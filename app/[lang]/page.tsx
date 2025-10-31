4c05e6837fc316243a00840bd5b71b91831de770
Hossein <Hoa.654123@gmail.com>
2025-10-31 17:46:45 +0330

    Fix: remove unused imports

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -2,7 +2,7 @@ import LocalizedLink from "@/components/localized-link";
 import { buttonVariants } from "@/components/ui/button";
 import { getDictionary, LangProps } from "@/lib/dictionaries";
 import { page_routes } from "@/lib/routes-config";
-import { MoveUpRightIcon, TerminalSquareIcon } from "lucide-react";
+import { MoveUpRightIcon } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home({ params }: LangProps) {
